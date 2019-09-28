80a61c0ff737d5c73b3b8eb6ae95bef5dcd64d7c
bartekkz <bartek.szmelczynski@gmail.com>
2019-09-28 10:49:52 +0200

    init flask app

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-'author: Bartekkz'
+from flask import Flask, render_template 
+import warnings
+warnings.filterwarnings('ignore')
 
-'''
-Flask deployment main file
-'''
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5002)
